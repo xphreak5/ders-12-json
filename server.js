7e2e6edf7c33ec9f3bdd3de678811e46413dc7e6
xphreak5 <yavuzdeveci5@gmail.com>
2022-03-23 20:16:23 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+// var msg = "Hello World"
+
+
+// function test() {
+//     console.log(msg)
+//     let msg = "test"
+//     console.log(msg)
+// }
+
+// test()
+
+
+// const names = ["Hello", "Malo", "Balo", "Bilo"]
+
+
+// names.shift()
+
+// console.log(names)
+
+// names.push("Mililo")
+// console.log(names)
+
+
+// for (let i = 1; i <= 7; ++i){
+//     console.log(i)
+// }
+
+// var x = 5
+
+// if (true) {
+//     let x = 10
+//     console.log(x)
+// }
+
+// console.log(x)
+
+var i = 1
+
+
+document.querySelector("button").addEventListener("click", () => {
+    console.log(i, "Kere tikladiniz")
+    i++
+})
+
+
+
